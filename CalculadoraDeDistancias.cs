fc672e5367bea7234f5b32cdffde4bf2dcb7da3d
Pedro Peixoto <pedro.ferreira@cloudkitchens.com>
2022-12-18 22:55:20 -0300

    primeira versão

---
+++ CalculadoraDeDistancias.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Globalization;
+
+namespace Questao1
+{
+    public class Testes
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+Olá, este programa recebe as distancias entre cidades e, com elas,
+consegue calcular distância de um caminho definido pelo usuário.
+");
+            var tabela = new CalculadoraDeDistancias();
+            tabela.CalcularDistanciaCaminho();
+        }
+    }
+    public partial class CalculadoraDeDistancias
+    {
+        decimal[,] arrayDistancias;
+
+        public CalculadoraDeDistancias()
+        {
+            int tamanhoArray = (int) ReceberInput(
+                message: "Por favor, comece entrando a quantidade de cidades:",
+                inputType: "int"
+            );
+
+            arrayDistancias = new decimal[tamanhoArray,tamanhoArray];
+
+            for (int i=0; i<tamanhoArray; i++)
+            {
+                for (int j=0; j< tamanhoArray; j++)
+                {
+                    if (i == j)
+                        arrayDistancias[i,j] = 0M;
+                    else if (j > i)
+                        arrayDistancias[i,j] = ReceberInput(
+                            message: $"\nPor favor, entre a distancia entre a cidade {i+1} e a cidade {j+1}:",
+                            inputType: "decimal"
+                        );
+                    else
+                        arrayDistancias[i,j] = arrayDistancias[j,i];
+                }
+            }
+        }
+        public void CalcularDistanciaCaminho()
+        {
+            List<int> caminho = new List<int>{};
+            string stringCaminho = "Início -> ";
+            int proximaCidade;
+            
+            do{
+                proximaCidade = (int) ReceberInput(
+                message: $"\n Por favor, entre a próxima cidade a ser visitada ou '0' para sair: \n {stringCaminho}",
+                inputType: "int"
+                );
+                //while loop pegando erros
+
+                if (proximaCidade == 0)
+                    break;
+                else if (proximaCidade > arrayDistancias.GetLength(0))
+                {
+                    Console.WriteLine($"\n\n {proximaCidade} não é o número de uma cidade válida!");
+                    continue;
+                }
+
+                caminho.Add(proximaCidade - 1);
+                stringCaminho += $" {proximaCidade} -> ";
+
+            }while(proximaCidade != 0);
+
+            Console.Clear();
+            Console.WriteLine($"O caminho escolhido foi: \n {stringCaminho.Substring(0,stringCaminho.Length-3)}");
+
+            decimal distancia = 0M;
+            
+            for (int i=1; i<caminho.Count; i++)
+            {   
+                distancia += this.arrayDistancias[
+                    (int) caminho[i], 
+                    (int) caminho[i-1]
+                ];
+            }
+
+            Console.WriteLine($"\n A distancia total do caminho definido é de {distancia}");
+        }
+    }
+}
---
+++ RecebimentoDeInputs.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+
+namespace Questao1
+{
+    public partial class CalculadoraDeDistancias
+    {
+        static decimal ReceberInput(string message, string inputType)
+        {
+            //Controle do do separador decimal
+            char decimalChar = Convert.ToChar(CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+            bool jaTemSeparador = false;
+
+            Console.Write(message + " 0");
+
+            List<char> listaInputs = new List<char> {};
+            ConsoleKey keyInfo = ConsoleKey.A;
+            decimal inputDecimal = 0M;
+            
+
+            while (keyInfo != ConsoleKey.Enter) {
+                
+                //Recebendo inputs do usuário
+                ConsoleKeyInfo key = Console.ReadKey();
+                keyInfo = key.Key;
+                
+                //inserindo inputs
+                if (Char.IsDigit(key.KeyChar) && listaInputs.Count<29)
+                {             
+                    listaInputs.Insert(listaInputs.Count, key.KeyChar);
+                }
+                else if (key.KeyChar == decimalChar && inputType == "decimal" && !jaTemSeparador)
+                {
+                    if (listaInputs.Count > 0)
+                    {
+                        listaInputs.Insert(listaInputs.Count, key.KeyChar);
+                        jaTemSeparador = true;
+                    }
+                    else
+                    {
+                        listaInputs.Insert(listaInputs.Count, '0');
+                        listaInputs.Insert(listaInputs.Count, key.KeyChar);
+                        jaTemSeparador = true; 
+                    }
+                }
+                //apagando inputs
+                else if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (listaInputs.Count>0)
+                    {
+                        if(listaInputs[listaInputs.Count-1] == decimalChar)
+                            jaTemSeparador = false;
+                        listaInputs.RemoveAt(listaInputs.Count -1);
+                    }
+                }
+                
+                Console.Clear();
+                Console.Write($"{message} {new string(listaInputs.ToArray())}");
+            } 
+
+            inputDecimal = ConverterInput(listaInputs);
+            return inputDecimal; 
+        }
+
+        static decimal ConverterInput(List<char> input)
+        {
+            string inputString = new string(input.ToArray());
+            inputString = inputString == "" ? "0" : inputString;
+            //tryParse para dizer que o númro é muito grande
+            decimal outputDecimal = decimal.Parse(inputString);
+            
+            return outputDecimal;
+        }
+    }
+}
