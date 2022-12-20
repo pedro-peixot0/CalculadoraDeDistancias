8a17be3b574eb8bc8761f7a61bf9e7ea788018bb
pedro-peixot0 <101193174+pedro-peixot0@users.noreply.github.com>
2022-12-20 17:28:32 -0300

    Update CalculadoraDeDistancias.cs

--- CalculadoraDeDistancias.cs
+++ CalculadoraDeDistancias.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Globalization;
 
 namespace Questao1
@@ -6,84 +6,48 @@ namespace Questao1
     public class Testes
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine(@"
-Olá, este programa recebe as distancias entre cidades e, com elas,
-consegue calcular distância de um caminho definido pelo usuário.
-");
+        {    
             var tabela = new CalculadoraDeDistancias();
             tabela.CalcularDistanciaCaminho();
         }
     }
     public partial class CalculadoraDeDistancias
     {
-        decimal[,] arrayDistancias;
-
-        public CalculadoraDeDistancias()
+        string mensagemInicial = @"
+Olá, este programa recebe as distancias entre cidades e, com elas,
+consegue calcular distância de um caminho definido pelo usuário.
+";   
+        private decimal[][] arrayDistancias;
+        private List<decimal> listaCaminho;
+  
+        //Os argumentos podem ser: "csv","default"
+        public CalculadoraDeDistancias(string tipoInputTabelaDistancias = "default", string tipoInputCaminho = "default")
         {
-            int tamanhoArray = (int) ReceberInput(
-                message: "Por favor, comece entrando a quantidade de cidades:",
-                inputType: "int"
-            );
-
-            arrayDistancias = new decimal[tamanhoArray,tamanhoArray];
-
-            for (int i=0; i<tamanhoArray; i++)
-            {
-                for (int j=0; j< tamanhoArray; j++)
-                {
-                    if (i == j)
-                        arrayDistancias[i,j] = 0M;
-                    else if (j > i)
-                        arrayDistancias[i,j] = ReceberInput(
-                            message: $"\nPor favor, entre a distancia entre a cidade {i+1} e a cidade {j+1}:",
-                            inputType: "decimal"
-                        );
-                    else
-                        arrayDistancias[i,j] = arrayDistancias[j,i];
-                }
-            }
+            Console.WriteLine(this.mensagemInicial);
+            arrayDistancias = ReceberTabelaDistanciasManualmente();
+            listaCaminho = ReceberTabelaCaminhoManualmente();
         }
-        public void CalcularDistanciaCaminho()
+        public CalculadoraDeDistancias(decimal[][] arrayDistancias, List<decimal> caminho)
         {
-            List<int> caminho = new List<int>{};
-            string stringCaminho = "Início -> ";
-            int proximaCidade;
-            
-            do{
-                proximaCidade = (int) ReceberInput(
-                message: $"\n Por favor, entre a próxima cidade a ser visitada ou '0' para sair: \n {stringCaminho}",
-                inputType: "int"
-                );
-                //while loop pegando erros
-
-                if (proximaCidade == 0)
-                    break;
-                else if (proximaCidade > arrayDistancias.GetLength(0))
-                {
-                    Console.WriteLine($"\n\n {proximaCidade} não é o número de uma cidade válida!");
-                    continue;
-                }
-
-                caminho.Add(proximaCidade);
-                stringCaminho += $" {proximaCidade} -> ";
-
-            }while(proximaCidade != 0);
-
-            Console.Clear();
-            Console.WriteLine($"O caminho escolhido foi: \n {stringCaminho.Substring(0,stringCaminho.Length-3)}");
-
+            Console.WriteLine(this.mensagemInicial);
+            this.arrayDistancias = arrayDistancias;
+            this.listaCaminho = caminho;
+        }
+        public decimal CalcularDistanciaCaminho()
+        {
             decimal distancia = 0M;
             
-            for (int i=1; i<caminho.Count; i++)
+            for (int i=1; i<this.listaCaminho.Count; i++)
             {   
-                distancia += this.arrayDistancias[
-                    (int) (caminho[i]-1), 
-                    (int) (caminho[i-1]-1)
-                ];
+                distancia += this.arrayDistancias
+                [(int) (this.listaCaminho[i]-1)]
+                [(int) (this.listaCaminho[i-1]-1)];
             }
-
+            
+            Console.WriteLine($"O caminho escolhido foi: \n {string.Join(" -> ",this.listaCaminho)}");
             Console.WriteLine($"\n A distancia total do caminho definido é de {distancia}");
+
+            return distancia;
         }
     }
 }
