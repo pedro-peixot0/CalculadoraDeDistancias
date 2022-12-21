5a4052e9ee3629b40eae122622aff563feefbd0e
pedro-peixot0 <101193174+pedro-peixot0@users.noreply.github.com>
2022-12-21 09:52:18 -0300

    Update CalculadoraDeDistancias.cs

--- CalculadoraDeDistancias.cs
+++ CalculadoraDeDistancias.cs
@@ -20,8 +20,8 @@ consegue calcular distância de um caminho definido pelo usuário.
         private decimal[][] arrayDistancias;
         private List<decimal> listaCaminho;
   
-        //Atualmente essa função só recebe o argumento "default", porém está estruturada de forma poder receber outros métodos futuramente
-        public CalculadoraDeDistancias(string tipoInputTabelaDistancias = "default", string tipoInputCaminho = "default")
+
+        public CalculadoraDeDistancias()
         {
             Console.WriteLine(this.mensagemInicial);
             arrayDistancias = ReceberTabelaDistanciasManualmente();
